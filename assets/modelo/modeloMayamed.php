b758c42fc345ecb1cceba7d357024591aa42307b
Axel Barragan <axelmontes92@gmail.com>
2016-10-26 22:54:54 -0500

    Avances generales

--- assets/modelo/modeloMayamed.php
+++ assets/modelo/modeloMayamed.php
@@ -39,8 +39,35 @@
       $sql = "INSERT INTO t_reservacion VALUES (null,'$nom','$ema','$hab','$perso','$checkin','$checkout','$fecha','$hora')";
       $res = $this->mysqli->query($sql);
       if($res) {
-        $respuesta = array("respuesta" => 'bien', "res" => 'Registro exitoso');
-        echo json_encode($respuesta);
+        /*$respuesta = array("respuesta" => 'bien', "res" => 'Registro exitoso');
+        echo json_encode($respuesta);*/
+        #Envío de correo para cliente
+        require 'PHPMailerAutoload.php';
+        $mail = new PHPMailer;
+        $mail->isSMTP();                                      // Set mailer to use SMTP
+        $mail->Host = ' smtp.gmail.com';                      // Specify main and backup SMTP servers
+        $mail->SMTPAuth = true;                               // Enable SMTP authentication
+        $mail->Username = 'axelmontes92@gmail.com';                 // SMTP username
+        $mail->Password = 'pandemiux64';                           // SMTP password
+        $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = 587;   
+
+        $mail->setFrom('from@example.com', 'Mailer');
+
+        $mail->addAddress('$ema');               // Name is optional
+        
+        $mail->Subject = 'Confirmación de reservación, Hotel Mayamed';
+        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+        if($mail->send()) {
+          /*echo 'Message has been sent';*/
+          $respuesta = array("respuesta" => 'bien', "res" => 'Registro exitoso');
+          echo json_encode($respuesta);
+        } else {
+          echo 'Message could not be sent.';
+          echo 'Mailer Error: ' . $mail->ErrorInfo; 
+        }
       } else {
         $respuesta = array("respuesta" => 'mal', "res" => 'Registro no completado');
         echo json_encode($respuesta);
